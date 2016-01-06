d6972620c0ca68ac0fe9df26ba3b64208a9a6e3a
TheCorbin <Ryanjcorbin@gmail.com>
2016-01-06 17:47:17 -0600

    Sane Date Validation

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,21 @@
 class Season < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :beginning_date, presence: true, uniqueness: true
+  validates :ending_date, presence: true, uniqueness: true
 
-  validates :name, presence: true
-  validates :beginning_date, presence: true
-  validates :ending_date, presence: true
+  validate :validate_sane_dates
+
+  private
+
+  def validate_sane_dates
+    if beginning_date.present? && ending_date.present?
+      if beginning_date == ending_date
+        errors.add(:ending_date, "must come after beginning date")
+      end
+
+      if ending_date < beginning_date
+        errors.add(:beginning_date, "must come before ending date")
+      end
+    end
+  end
 end
--- spec/models/season_spec.rb
+++ spec/models/season_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Season, type: :model do
-  # validates presence of: name, beginning and ending_dates
-  # validates uniqueness of name
-  it { should validate_presence_of(:name) }
+  context 'validation' do
+    let(:subject) { create :season }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:beginning_date) }
+    it { is_expected.to validate_presence_of(:ending_date) }
+
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:beginning_date) }
+    it { is_expected.to validate_uniqueness_of(:ending_date) }
+
+    describe 'validate_sane_dates' do
+      context 'ending_date after beginning_date' do
+        let(:subject) { build :season, beginning_date: Date.yesterday, ending_date: Date.today }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'ending_date same as beginning_date' do
+        let(:subject) { build :season, beginning_date: Date.today, ending_date: Date.today }
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'ending_date before beginning_date' do
+        let(:subject) { build :season, beginning_date: Date.today, ending_date: Date.yesterday }
+        it { is_expected.not_to be_valid }
+      end
+
+    end
+  end
 end
