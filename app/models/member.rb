6ab39c3af8a718bb5527c22de99bd84a6b507b51
TheCorbin <Ryanjcorbin@gmail.com>
2016-01-08 18:23:20 -0600

    email validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
   validates :address, presence: true, uniqueness: true
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -12,12 +12,60 @@ RSpec.describe Member, type: :model do
   context 'validation' do
     let(:subject) { create :member }
 
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.not_to validate_presence_of(:month) }
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:address) }
+    describe 'name' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_uniqueness_of(:name) }
+    end
 
-    it { is_expected.to validate_uniqueness_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:email) }
+    describe 'month' do
+      it { is_expected.not_to validate_presence_of(:month) }
+    end
+
+    describe 'email' do
+      let(:member) {build :member, email: email}
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email) }
+
+      context 'when valid' do
+        let(:email) { 'valid_email@example.com' }
+        it "is valid" do
+          expect(member).to be_valid
+        end
+      end
+
+      context 'missing @ symbol' do
+        let(:email) { 'no_at_sign_example.com' }
+        it 'is invalid' do
+          expect(member).not_to be_valid
+        end
+      end
+
+      context 'without suffix' do
+        let(:email) { 'no_suffix@example' }
+        it 'is invalid' do
+          expect(member).not_to be_valid
+        end
+      end
+
+      context 'without prefix' do
+        let(:email) { '@example.com' }
+        it 'is invalid' do
+          expect(member).not_to be_valid
+        end
+      end
+
+      context 'illegal characters' do
+        let(:email) { 'bad_;characters@example.com' }
+        it 'is invalid' do
+          expect(member).not_to be_valid
+        end
+      end
+    end
+
+    describe 'address' do
+      it { is_expected.to validate_presence_of(:address) }
+    end
+
+
   end
 end
