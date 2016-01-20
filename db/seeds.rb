f743a45ad363d8378043b13e8f4d806171b7fd26
Ryan Corbin <ryan@bendyworks.com>
2016-01-19 19:51:29 -0600

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+12.times do |i|
+  Month.create! order: i, name: Date::MONTHNAMES.compact[i]
+end
